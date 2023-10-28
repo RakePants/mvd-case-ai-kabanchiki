e3aed658c052f50b68f10f913f3d6c3060008d84
sh6abra <andpro22223@gmail.com>
2023-10-29 01:12:10 +0500

    Фронтенд

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,3 @@
-const { relative } = require("path");
-
 const imageUploader = document.getElementById("imageUploader");
 const fileInput = document.getElementById("fileInput");
 const oldDesc = document.querySelector('.main__description');
@@ -10,23 +8,30 @@ const backBtn = document.getElementById('backBtn');
 const type = document.getElementById("typed");
 const info = document.getElementById("typed-strings").querySelector('span');
 const mainButton = document.querySelector(".main__buttons");
+const newImgButton = document.getElementById("newImg_button");
+
 
 let typed = new Typed('#typed', {
   stringsElement: '#typed-strings',
   typeSpeed: 20, // Скорость печати
-  startDelay: 500, // Задержка перед стартом анимации // Скорость удаления
+  startDelay: 500, // Задержка перед стартом анимации // 
   fadeOut: false,
   showCursor: false,
   loop: false
 });
+fileInput.value = '';
 
-let flag = true;
+newImgButton.addEventListener("click", function() {
+  fileInput.value = '';
+  fileInput.click();
+});
+
 imageUploader.addEventListener("click", function() {
   if(fileInput.files.length === 0){
-    flag = false;
     fileInput.click();
   }
 });
+
 backBtn.addEventListener("click", function(){
   newDesc.classList.remove("animate__fadeInRight");
   newDesc.classList.add("animate__fadeOutRight");
@@ -44,43 +49,39 @@ backBtn.addEventListener("click", function(){
   type.innerHTML = '';
   typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
-    typeSpeed: 60, // Скорость печати
-    startDelay: 0, // Задержка перед стартом анимации // Скорость удаления
+    typeSpeed: 20, // Скорость печати
+    startDelay: 10, // Задержка перед стартом анимации // Скорость удаления
     fadeOut: false,
     showCursor: false,
     loop: false
   });
-
-  
 });
-fileInput.addEventListener("change", function() {
-    loadAnimation();
-    const selectedFile = fileInput.files[0];
-    if (selectedFile) {
-      // Проверка, что выбран только один файл
-      if (fileInput.files.length === 1) {
-        const fileReader = new FileReader();
-        fileReader.onload = function(event) {
-          imageUploader.innerHTML = "";
-          const newImage = document.createElement("img");
-          newImage.src = event.target.result;
-          imageUploader.appendChild(newImage);
-          mainButton.style.display = 'flex';
-          mainButton.classList.remove("animate__fadeOutDown");
-          mainButton.classList.add("animate__fadeInUp");
-          // sendImageToBackend(selectedFile);
-          
-        };
-        fileReader.readAsDataURL(selectedFile);
-      } else {
-        alert("Пожалуйста, выберите только один файл.");
-        // Очистить input
-        fileInput.value = "";
-      }
 
-      
-    }
-  });
+
+fileInput.addEventListener("change", function() {
+  loadAnimation();
+  const selectedFile = fileInput.files[0];
+  if (selectedFile) {
+    // Проверка, что выбран только один файл
+    if (fileInput.files.length === 1) {
+      const fileReader = new FileReader();
+      fileReader.onload = function(event) {
+        imageUploader.innerHTML = "";
+        const newImage = document.createElement("img");
+        newImage.src = event.target.result;
+        imageUploader.appendChild(newImage);
+        mainButton.style.display = 'flex';
+        mainButton.classList.remove("animate__fadeOutDown");
+        mainButton.classList.add("animate__fadeInUp");   
+      };
+      fileReader.readAsDataURL(selectedFile);
+    } else {
+      alert("Пожалуйста, выберите только один файл.");
+      // Очистить input
+      fileInput.value = "";
+    }      
+  }
+});
 
 function sendImageToBackend() {
   imageFile = fileInput.files[0];
@@ -89,7 +90,6 @@ function sendImageToBackend() {
   
   const formData = new FormData();
   formData.append("image", imageFile);
-  updateInfo();
   fetch("http://localhost:1001/photo", {
     method: "POST",
     body: formData,
@@ -106,35 +106,54 @@ function sendImageToBackend() {
           showCursor: false,
           loop: false
         });
+      } else if(!response.ok){
+        // throw new Error('Ошибка при загрузке архива');
+      }else if(response.ok){
+        return response.arrayBuffer();
       }
-      
-      return response.arrayBuffer();
     })
     .then(zipData => {
       return JSZip.loadAsync(zipData);
     })
     .then(zip => {
+      let counter = 0;
       zip.forEach((relativePath, file) => {
+        counter++;
         file.async('blob').then(blobData => {
           const imageUrl = URL.createObjectURL(blobData);
           updateInfo(imageUrl);
-
         })
       });
+      
     })
     .catch(error => {
       console.error("Произошла ошибка при выполнении fetch запроса:", error);
     });
+    var counterText = document.getElementById("counter-strings").querySelector("span");
+    counterText.innerHTML = "Обнаруженных лиц с оружием: " +  2;
+    document.getElementById("count").innerHTML = '';
+    typed = new Typed('#count', {
+        stringsElement: '#counter-strings',
+        typeSpeed: 30, // Скорость печати
+        startDelay: 1000, // Задержка перед стартом анимации // Скорость удаления
+        fadeOut: false,
+        showCursor: false,
+        loop: false
+    });
+
     startPage.classList.remove("animate__fadeInLeft");
     startPage.classList.add("animate__fadeOutLeft");
     setTimeout(function(){
-       newDesc.classList.remove("animate__fadeOutRight");
-       newDesc.classList.add("animate__fadeInRight");
-       newDesc.style.display = 'flex';
-       mainButton.classList.remove("animate__fadeInUp");
-       mainButton.classList.add("animate__fadeInOut"); 
-       mainButton.style.display = "none";
+        newDesc.classList.remove("animate__fadeOutRight");
+        newDesc.classList.add("animate__fadeInRight");
+        newDesc.style.display = 'flex';
+        mainButton.classList.remove("animate__fadeInUp");
+        mainButton.classList.add("animate__fadeInOut"); 
+        mainButton.style.display = "none";
     }, 500);
+    worm.style.animation = 'start 2s';
+    worm.style.width = '100%';
+
 }
 
 function updateInfo(imageUrl){
@@ -150,21 +169,19 @@ function updateInfo(imageUrl){
   newCol.appendChild(newPerson);
   newDesc.appendChild(newCol);
   newImg.src = imageUrl;
+
   newCol.classList.add("col-md-4");
   newPerson.classList.add("output__person", "wow", "animate__animated", "animate__fadeInUp");
   newFace.classList.add("face");
   newPercent.classList.add("percent");
 
 
-  newPercent.innerHTML = "Вероятность 30%";
-  
-
- 
+  newPercent.innerHTML = "Вероятность 30%"; 
 }
 
 function loadAnimation(){
   worm.style.transition = "2s all";
-  worm.style.width = "300px";
+  worm.style.animation = "toLoad 1s";
   setTimeout(()=> worm.style.animation = "load 1.2s infinite", 1000);
   
 }
